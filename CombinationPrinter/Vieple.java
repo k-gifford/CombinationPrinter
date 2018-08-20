498c8efce090eaa0e1c299b6a3dafbea1e10a74b
Kealan Gifford <kealan.gifford@gmail.com>
2018-08-20 14:02:19 +0100

    Refactored to allow object creation of Vieple combination printer

--- CombinationPrinter/Vieple.java
+++ CombinationPrinter/Vieple.java
@@ -3,89 +3,108 @@ import java.util.TreeSet;
 /**
  *
  * @author Kealan Gifford
- * 
+ *
  */
 public class Vieple {
 
-    /**
-     * @param args none
+    boolean getting_characters = true;
+    boolean getting_maxlength = true;
+    Scanner userin = new Scanner(System.in);
+    String characterString = null;
+    char[] characters = null;
+    int maxlength = 0;
+
+    /** At first, I used a HashSet to remove duplicates but switched to
+     * TreeSet to maintain ordering.
      */
-    public static void main(String[] args) {
-        boolean getting_characters = true;
-        boolean getting_maxlength = true;
-        Scanner userin = new Scanner(System.in);
-        String characterString = null;
-        char[] characters = null;
-        int maxlength = 0;
-        
-        System.out.println("Enter any characters (no spaces):");
-        while(getting_characters){
-            characterString = userin.nextLine();
-            if(characterString.length() > 0){
-                characters = new char[characterString.length()];
-                for(int i = 0; i < characterString.length(); i++){
-                    characters[i] = characterString.charAt(i);
-                }
-                getting_characters = false;
-            }
-            else{
-                System.out.println("Enter four characters (no spaces):");
-            }
-        }
+    TreeSet<String> output = new TreeSet<>();
+
+    /**
+     * Constructor.
+     */
+     public Vieple(){
+       setCharacterString();
+       setComboLength();
+       this.output = combinations(characters, output, "", maxlength);
+     }
+
+    /**
+     * Gets string from user in order to carry out combination checks.
+     */
+     public void setCharacterString(){
+       System.out.println("Enter any String of characters (no spaces):");
+       while(this.getting_characters){
+           this.characterString = userin.nextLine();
+           if(characterString.length() > 0){
+               this.characters = new char[characterString.length()];
+               for(int i = 0; i < characterString.length(); i++){
+                   this.characters[i] = characterString.charAt(i);
+               }
+               this.getting_characters = false;
+           }
+           else{
+               System.out.println("Enter any String of characters (no spaces):");
+           }
+       }
+     }
+
+     /**
+      * Sets max combination output string length.
+      */
+      public void setComboLength(){
         System.out.println("Enter max string length of combinations (>0):");
-        while(getting_maxlength){
-            maxlength = userin.nextInt();
-            if(maxlength > 0){
-                getting_maxlength = false;
+        while(this.getting_maxlength){
+            this.maxlength = userin.nextInt();
+            if(this.maxlength > 0){
+                this.getting_maxlength = false;
             }
             else{
-                System.out.println("Enter max string length of combinations(>0):");   
+                System.out.println("Enter max string length of combinations(>0):");
             }
         }
-        
-        System.out.println("All combinations of " + characterString + ":");        
-        
-        /** At first, I used a HashSet to remove duplicates but switched to 
-         * TreeSet to maintain ordering for ease of verification
-         */
-        TreeSet<String> output = new TreeSet<>();   
-        
-        output = combinations(characters, output, "", maxlength);
-        
+      }
+
+      /**
+       * Print out the combinations, in order.
+       */
+      public void printCombos(){
+
+        System.out.println("All combinations of " + this.characterString + ":");
         // print each combination string in the TreeSet
-        for(String s : output){
+        for(String s : this.output){
             System.out.println(s);
         }
-    }
+      }
+
     /**
-     * 
+     *
      * @param characters alphabet of characters to be used in generating combos
      * @param output TreeSet to store all the generated combinations
      * @param combo an individual combination (start with an empty string)
      * @param maxlength maximum length allowed for a combination string
-     * @return 
+     * @return
      */
-    public static TreeSet<String> combinations(char[] characters, TreeSet<String> output, String combo, int maxlength){
-        
+    public TreeSet<String> combinations(char[] characters, TreeSet<String> output, String combo, int maxlength){
+
         // if the string combination has reached max length given
-        if(combo.length() == maxlength){
-            
+        if(combo.length() == this.maxlength){
+
             // System.out.println(combo);
-            
-            /** 
+
+            /**
              * printed here initially but wanted to remove the printing of
-             * duplicate combinations. e.g. if the letters entered were all 
+             * duplicate combinations. e.g. if the letters entered were all
              * the same ('aaaa') then it would print aaaaa, aaaaa, aaaaa ... etc
-             * so simply stored the combos in a TreeSet for ordering and 
+             * so simply stored the combos in a TreeSet for ordering and
              * ignoring of duplicates
              */
             output.add(combo);
             return(output);
         }
-        
+
         /**
          * Otherwise keep placing each character in the given set at the start
-         * of the combo string and then recurse for the rest of this combo string 
+         * of the combo string and then recurse for the rest of this combo string
          * (doing exactly the same thing but simply adding to the current combo)
         */
         for(char c : characters){
@@ -93,5 +112,15 @@ public class Vieple {
             combinations(characters, output, nextCombo, maxlength);
         }
        return(output);
-    }  
+    }
+
+    /**
+     * @param args none
+     */
+    public static void main(String[] args) {
+
+      Vieple combinator = new Vieple();
+
+      combinator.printCombos();
+    }
 }
